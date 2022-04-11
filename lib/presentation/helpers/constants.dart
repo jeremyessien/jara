0135e055a4165560c2aad424d1b9e215500a16cf
Jeremiah Ekanem <jeremyessien@gmail.com>
2022-04-11 15:49:22 +0100

    Update constants.dart

--- lib/presentation/helpers/constants.dart
+++ lib/presentation/helpers/constants.dart
@@ -76,3 +76,8 @@ OutlineInputBorder outlineInputBorder() {
     borderSide: const BorderSide(color: kGrey),
   );
 }
+const OutlineInputBorder koutlineInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(50)),
+    borderSide: BorderSide(color: Colors.grey, width: 1, style: BorderStyle.solid)
+);
+
