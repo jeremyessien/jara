b6faaf32653caf6cc814b1d673674f7f27fecc66
Jeremiah Ekanem <jeremyessien@gmail.com>
2022-04-11 16:02:30 +0100

    Update homePage.dart

--- lib/presentation/screens/homePage/homePage.dart
+++ lib/presentation/screens/homePage/homePage.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:jara/presentation/screens/Authentication/home/components/body.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return const HomeBody();
